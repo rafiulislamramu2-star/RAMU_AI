b88ff0a397a103cd78391edebfede8bcbb8522cd
rafiulislamramu2-star <rafiulislamramu2@gmail.com>
2026-08-04 01:49:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-Create responsive RAMU ORBIT home page
+// Sidebar Menu
+function toggleMenu() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
+// Live Clock
+function updateClock() {
+    const now = new Date();
+
+    const options = {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true
+    };
+
+    document.getElementById("clock").innerHTML =
+        now.toLocaleTimeString("en-US", options);
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+// Weather (Location)
+const weatherText = document.getElementById("weatherText");
+
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(
+
+        function(position) {
+
+            const lat = position.coords.latitude.toFixed(4);
+            const lon = position.coords.longitude.toFixed(4);
+
+            weatherText.innerHTML =
+                "📍 Latitude: " + lat +
+                "<br>🌍 Longitude: " + lon;
+
+        },
+
+        function() {
+            weatherText.innerHTML =
+                "❌ Location Permission Denied";
+        }
+
+    );
+
+} else {
+
+    weatherText.innerHTML =
+        "Geolocation is not supported.";
+
+}
